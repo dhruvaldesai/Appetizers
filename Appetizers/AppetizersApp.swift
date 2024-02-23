78f31ea4962a8a702152bb8c30f402825672595e
dhruvaldesai <dhruvaldesai1997@gmail.com>
2024-02-23 20:01:27 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Dhruval Desai on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
